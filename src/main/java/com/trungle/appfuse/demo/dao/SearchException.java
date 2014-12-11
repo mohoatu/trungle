23fd3a860c59f698aa6d2f81f8c8286b196002f2
mohoatu <mohoatu@gmail.com>
2014-12-11 17:08:40 +0700

    demo

---
+++ src/main/java/com/trungle/appfuse/demo/dao/SearchException.java
@@ -0,0 +1,13 @@
+package com.trungle.appfuse.demo.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
